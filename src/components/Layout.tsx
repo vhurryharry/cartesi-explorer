7b24fd6d71de1cc5470e1a2fa653c0536a9e5a1f
Alexander Bai <alexander.bai@cartesi.io>
2021-03-10 10:23:17 -0500

    Add unsupported browser warning

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 // PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
 import NavBar from './NavBar';
 import Footer from './Footer';
@@ -17,6 +17,7 @@ import { useBlockNumber } from '../services/eth';
 import useMeta from '../graphql/hooks/useMeta';
 
 const threshold = 25;
+declare const InstallTrigger: any;
 
 const SyncStatus = () => {
     const blockNumber = useBlockNumber();
@@ -39,6 +40,48 @@ const SyncStatus = () => {
     );
 };
 
+const UnsupportedBrowser = () => {
+    const [isBrave, setIsBrave] = useState(true);
+
+    // Chrome
+    const isChrome =
+        !!window['chrome'] &&
+        (!!window['chrome'].webstore || !!window['chrome'].runtime);
+
+    // Firefox 1.0+
+    //noinspection TypeScriptUnresolvedVariable
+    const isFirefox = typeof InstallTrigger !== 'undefined';
+
+    // Internet Explorer 6-11
+    const isIE = /*@cc_on!@*/ false || !!document['documentMode'];
+
+    // Edge 20+
+    const isEdge = !isIE && !!window.StyleMedia;
+
+    useEffect(() => {
+        const checkIsBrave = async () => {
+            // Brave
+            setIsBrave(
+                (navigator['brave'] && (await navigator['brave'].isBrave())) ||
+                    false
+            );
+        };
+
+        checkIsBrave();
+    }, []);
+
+    if (!isChrome && !isFirefox && !isEdge && !isBrave) {
+        return (
+            <div className="layout-content-issue">
+                <i className="fas fa-exclamation-triangle"></i> Some functions
+                may not work as expected on unsupported browsers. Supported
+                browsers: Chrome, Firefox, Brave, Microsoft Edge
+            </div>
+        );
+    }
+    return <div />;
+};
+
 const LayoutComponent = ({ children, className = '' }) => {
     const { error } = useWeb3React();
     const isUnsupportedChainIdError = error instanceof UnsupportedChainIdError;
@@ -52,6 +95,7 @@ const LayoutComponent = ({ children, className = '' }) => {
             <div>
                 {!isUnsupportedChainIdError && (
                     <div className="layout-content">
+                        <UnsupportedBrowser />
                         <SyncStatus />
                         {children}
                     </div>
